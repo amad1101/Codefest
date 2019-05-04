251e9c06f08fbc0b603d1362c4684cbf406f5ecf
Amadou Diallo <ad3464@drexel.edu>
2019-05-04 19:06:15 -0400

    fixed

--- src/views/Simulation.vue
+++ src/views/Simulation.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="simulation">
     <HelloWorld></HelloWorld>
-
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <input @click="toggleTodoDone(index)" type="checkbox" :checked="todo.done">
+        <span :class="{ done: todo.done }">{{todo.title}}</span>
+      </li>
+    </ul>
 
 
   </div>
@@ -9,12 +14,28 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import { mapState, mapMutations, mapActions } from 'vuex';
 
 export default {
   name: 'simulation',
   components: {
     HelloWorld
+  },
+  computed: {
+    newTodo: {
+      get() {
+        return this.$store.state.job;
+      },
+      set(value) {
+        this.$store.commit('setNewTodo', value);
+      }
+    },
+    ...mapState(['title', 'todos'])
+  },
+  methods: {
+    ...mapMutations(['allDone']),
+    ...mapActions(['updateTracker'])
   }
 }
 </script>
